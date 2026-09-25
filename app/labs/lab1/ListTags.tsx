df9e291ac989bcc040db81e8656c680a88be59dd
Melanie Lochrie <mlochrie@gmail.com>
2026-09-25 15:18:38 -0700

    Complete Lab 1 List Tags

---
+++ app/labs/lab1/ListTags.tsx
@@ -0,0 +1,53 @@
+export default function ListTags() {
+    return (
+        <div id="wd-lists">
+            <h4>List Tags</h4>
+            <h5>Ordered List Tag</h5>
+            How to make pancakes:
+            <ol id="wd-pancakes">
+                <li>Mix dry ingredients.</li>
+                <li>Add wet ingredients.</li>
+                <li>Stir to combine.</li>
+                <li>Heat a skillet or griddle.</li>
+                <li>Pour batter onto the skillet.</li>
+                <li>Cook until bubbly on top.</li>
+                <li>Flip and cook the other side.</li>
+                <li>Serve and enjoy!</li>
+            </ol>
+            <ol id="wd-your-favorite-recipe">
+                <li>Boil water.</li>
+                <li>Sift the match with a fine sieve.</li>
+                <li>Pour matcha into a bowl with hot water.</li>
+                <li>Whisk.</li>
+                <li>Pour over milk and ice.</li>
+                <li>Mix and enjoy!</li>
+            </ol>
+            <h5>Unordered List Tag</h5>
+            My favorite books (in no particular order)
+            <ul id="wd-my-books">
+                <li>Dune</li>
+                <li>Lord of the Rings</li>
+                <li>Ender&apos;s Game</li>
+                <li>Red Mars</li>
+                <li>The Forever War</li>
+            </ul>
+            My favorite books (in no particular order)
+            <ul id="wd-your-books">
+                <li>The Silent Patient</li>
+                <li>Throne of Glass</li>
+                <li>My Husband's Wife</li>
+                <li>Gone Girl</li>
+                <li>A Thousand Splendid Suns</li>
+            </ul>
+            Common HTML tags from this chapter
+            <ul id="wd-ai-html-tags">
+                <li>h1 – the largest heading</li>
+                <li>p – a paragraph of text</li>
+                <li>ol – an ordered, numbered list</li>
+                <li>ul – an unordered, bulleted list</li>
+                <li>li – an item inside a list</li>
+                <li>table – rows and columns of data</li>
+            </ul>
+        </div>
+    );
+}
