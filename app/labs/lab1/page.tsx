73c865863ed1f40fd6f80c42ee34149179a2362a
Melanie Lochrie <mlochrie@gmail.com>
2026-09-25 14:57:39 -0700

    Add ParagraphTag to Lab 1

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -7,6 +7,8 @@ export default function Lab1() {
             <h2>Lab 1</h2>
             <h3>HTML Examples</h3>
             <HeadingTags />
+            <ParagraphTag />
+
             {/* do the next exercise here */}
         </div>
     );
